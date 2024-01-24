1255c4017a2a3ff9a8fdb55e76515f55c96d3542
ernesttheiss <theissernest@ymail.com>
2024-01-24 14:49:01 -0800

    MyFirstPush

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Account from './components/Account';
+import Books from './components/Books';
+import Navbar from './components/Navigations';
+import Register from './components/Register';
+import SingleBook from './components/SingleBook';
+import Login from './components/Login';
 
-import './App.css'
 
-function App() {
 
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/account" component={Account} />
+          <Route path="/books" component={Books} />
+          <Route path="/register" component={Register} />
+          <Route path="/book/:id" component={SingleBook} />
+          <Route path="/login" component={Login}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,5 @@
+const SingleBook = () => {
+
+};
+
+export default SingleBook;
